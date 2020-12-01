b774d9a40be54e08bd26a2ae7bb3d0d81fadef7c
n0m1s <git@homnomnom.fr>
2020-12-01 16:34:14 +0100

    prettier display of face label

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 
 #include "log.h"
 
+void drawCenteredText(cv::Mat& img, std::string const& text, cv::Point const& anchor, cv::Scalar const& color)
+{
+    int const    fontFace  = cv::FONT_HERSHEY_SIMPLEX;
+    double const fontScale = 1.0;
+    int const    thickness = 1;
+
+    int baseLine;
+
+    cv::Size textSize = cv::getTextSize(text, fontFace, fontScale, thickness, &baseLine);
+
+    cv::Point pos = anchor + cv::Point(-textSize.width/2, textSize.height/2);
+    cv::putText(img, text, pos, fontFace, fontScale, color);
+}
+
 int main(int argc, char** argv) {
     Log::ensure(argc == 3) << "Usage: " << argv[0] << " <input> <output>";
 
@@ -22,12 +36,23 @@ int main(int argc, char** argv) {
     std::vector<cv::Rect> faces;
     face_cascade.detectMultiScale(img, faces);
 
-    for(cv::Rect faceRect: faces)
+    for(size_t i = 0; i < faces.size(); ++i)
     {
-        auto color = cv::Scalar(255, 0, 255);
+        cv::Rect const& faceRect = faces[i];
+
+        cv::Scalar const white(255, 255, 255);
+        cv::Scalar const magenta(255, 0, 255);
 
         cv::Point center(faceRect.x + faceRect.width/2, faceRect.y + faceRect.height/2 );
-        cv::ellipse(img, center, cv::Size(faceRect.width/2, faceRect.height/2), 0, 0, 360, color, 4);
+        cv::Size size(faceRect.width/2, faceRect.height/2);
+
+        std::vector<cv::Point> circlePts;
+        cv::ellipse2Poly(center, size, 0, 0, 360, 10, circlePts);
+
+        cv::fillConvexPoly(img, circlePts, white);
+        cv::polylines(img, circlePts, true, magenta, 4);
+
+        drawCenteredText(img, std::to_string(i), center, magenta);
     }
 
     cv::imwrite(outputFilename, img);
